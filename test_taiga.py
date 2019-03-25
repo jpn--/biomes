0a5964e489e22b90a75e3b51363ee1a89621abaf
Jeff Newman <jeff@newman.me>
2019-03-25 10:20:40 -0500

    test imports not full geopandas package

---
+++ test_taiga.py
@@ -0,0 +1,22 @@
+
+def test_imports():
+	import numpy
+	import pandas
+	import larch
+	import shapely
+	import geopandas
+	import matplotlib
+	import seaborn
+	import pydot
+	import osmnx
+	import contextily
+	import tqdm
+	import ipywidgets
+	import joblib
+	import dask
+	import distributed
+	import cloudpickle
+	import xmle
+	import filez
+	import addict_yaml
+
