9b9aed334596dd6278cd7e0e4245b6ebaa3e03ad
Jeff Newman <jeff@newman.me>
2019-03-25 12:42:14 -0500

    tt

---
+++ test_tt.py
@@ -0,0 +1,20 @@
+
+def test_imports():
+	import numpy
+	import pandas
+	import larch
+	import shapely
+	import geopandas
+	import matplotlib
+	import seaborn
+	import pydot
+	import osmnx
+	import contextily
+	import tqdm
+	import ipywidgets
+	import joblib
+	import cloudpickle
+	import xmle
+	import filez
+	import addict_yaml
+
