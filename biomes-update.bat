07968a8dd4d6a86bd1afeb5291352849c58ef6a1
Jeffrey Newman <jeff@newman.me>
2019-03-30 10:13:00 -0500

    windows install and update batch scripts

---
+++ biomes-update.bat
@@ -0,0 +1,19 @@
+call conda update -y -n base -c defaults conda
+
+call conda env update jpn/tower --prune
+
+call conda activate tower
+set NODE_OPTIONS=--max-old-space-size=4096
+jupyter labextension install @jupyterlab/toc --no-build
+jupyter labextension install @jupyter-widgets/jupyterlab-manager --no-build
+jupyter labextension install jupyterlab_bokeh --no-build
+jupyter labextension install @jupyterlab/plotly-extension --no-build
+jupyter labextension install @jupyterlab/geojson-extension --no-build
+jupyter labextension install @jupyterlab/mathjax3-extension --no-build
+jupyter labextension install plotlywidget --no-build
+jupyter lab build
+call conda deactivate
+
+call conda env update jpn/taiga
+call conda env update camsys/tt
+
